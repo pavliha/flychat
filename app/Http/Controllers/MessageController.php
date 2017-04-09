802cd11765f9b724b25f98f6f6395343c0e0e772
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2017-04-09 05:27:31 +0300

    add middlewares

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,10 @@ use App\Message;
 
 class MessageController extends Controller
 {
+    public function __construct() {
+        $this->middleware("auth");
+    }
+
     public function index(){
 
         $messages = Message::with("user")->get();
