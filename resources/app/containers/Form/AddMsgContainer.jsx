cffac29afa55c0740e4840cbe1c67f6ef86b8e3b
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2017-10-03 13:34:24 +0300

    Update AddMsgContainer.jsx

--- resources/app/containers/Form/AddMsgContainer.jsx
+++ resources/app/containers/Form/AddMsgContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import msg from "../../actions/msg"
-
+//*************Crowdbotics*******************//
 @connect(store => {
     return {
         msg: store.msg
